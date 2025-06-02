59b8262804433cddb7608b50154c9fb0ff359378
DarshanNagesh10 <darshann.801f@gmail.com>
2025-06-02 19:14:04 +0530

    Initial commit on dev branch

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // change to prod URL later
+  // baseURL: 'http://localhost:5000/api', 
+  baseURL: 'http://ec2-3-6-233-101.ap-south-1.compute.amazonaws.com:5000/api', 
 });
 
 export default instance;
